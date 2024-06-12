94b42cd31b3f962612a6521379be770daf2c8e3a
obliguedon <None>
2024-06-12 09:46:25 +0200

    [WIP] POC of the full flow

--- sources/main.py
+++ sources/main.py
@@ -4,6 +4,8 @@ import yaml
 import pathlib
 import shutil
 import os
+from packages import yaml_utils
+from packages import template_utils
 
 #  ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗
 # ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝
@@ -61,17 +63,10 @@ parser.add_argument('--dump-config',
                     help="the the CLI arguments take priority over the config file")
 
 parser.add_argument('--dump-format',
-                    metavar="list of file name in default output folder" #TODO
+                    metavar="list of file name in default output folder", #TODO
                     nargs='?',
                     type=str,
                     help="dump an example of output script")
-
-# maybe not necessary to have this argument if the file is already specified
-parser.add_argument('--format-dir', '-fd',
-                    metavar=config_yaml["format-dir"],
-                    nargs='?',
-                    type=str,
-                    help="the directory where the output format script are stored")
 
 parser.add_argument('--output-format', '-of',
                     metavar=config_yaml["output-format"],
@@ -82,7 +77,7 @@ parser.add_argument('--output-format', '-of',
 parser.add_argument('--output-file', '-o',
                     )
 
-args = parser.parse_args
+args = parser.parse_args()
 
 
 # ██╗      ██████╗  ██████╗  ██████╗ ██╗███╗   ██╗ ██████╗
@@ -99,3 +94,51 @@ logging.basicConfig(level=numeric_level)
 
 if args.dump_config == True:
     shutil.copy(abs_default_config, os.getcwd())
+
+
+# ████████╗██╗  ██╗███████╗     █████╗ ██████╗ ██████╗
+# ╚══██╔══╝██║  ██║██╔════╝    ██╔══██╗██╔══██╗██╔══██╗
+#    ██║   ███████║█████╗      ███████║██████╔╝██████╔╝
+#    ██║   ██╔══██║██╔══╝      ██╔══██║██╔═══╝ ██╔═══╝
+#    ██║   ██║  ██║███████╗    ██║  ██║██║     ██║
+#    ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═╝╚═╝     ╚═╝
+
+all_items = dict()
+
+# get all the keys that need to be extracted from the YAML files by reading and listing the
+# variables in the output template
+if args.output_format != None:
+    abs_template = pathlib.Path(args.output_format).resolve()
+    template = str()
+    with open(abs_template, 'r') as file:
+        template = file.read()
+    
+    all_vars = template_utils.extractor.get_all_params(template)
+    all_keys = template_utils.extractor.get_all_values(all_vars)
+
+    if args.top_yaml != None:
+        print(f"top YAML is {args.top_yaml}")
+        abs_top_yaml = pathlib.Path(args.top_yaml[0]).resolve()
+        all_deps = yaml_utils.extractor.get_deps(abs_top_yaml)
+
+        for key in all_keys:
+             all_items[key] = yaml_utils.extractor.get_files(all_deps, item=key, flag=args.flag)
+
+        print (f"ALL_ITEMS = {all_items}")
+        print (f"ITEMS['sources'] = {all_items["sources"]}")
+        print (f"ALL_VARS['sources'] = {all_vars["sources"]}")
+        output = template_utils.generator.fill_template(template, all_items)
+        print(f"## OUTPUT = {output}")
+    else:
+        logging.error("top YAML file not specified")
+
+
+# extract those key from the YAML files and put all of those in a dictionnary
+
+# send the dictionnary to the output_generator to fill the variables in the template
+
+# get back the output string and write it to a file
+# if the output file isn't specified, print the output
+# if the template isn't specified, simply print a list of everything, or just the specified key or flag.
+
+
