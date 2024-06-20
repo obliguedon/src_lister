527c35c2457e966da596d2cb2933b5c1d3fa8949
obliguedon <None>
2024-06-20 11:07:14 +0200

    try fixing CI

--- src/files_lister/template_utils/extractor/get_all_params.py
+++ src/files_lister/template_utils/extractor/get_all_params.py
@@ -2,10 +2,13 @@ import re
 import typing
 
 def get_all_params(template: str) -> typing.Dict[str, typing.List[re.Match]]:
+    
+    if not isinstance(template, str):
+        raise TypeError("template must be a string")
     # get all match in the template
     params = dict()
 
-    pattern = re.compile(r"(?:{{1,2}(?:(?P<prefix>[a-zA-z0-9\-\+\_\\]+){)?(?P<key>[a-zA-z0-9\-]*)(?:\=(?P<extension>\.[a-zA-z0-9]+))?(?:}(?P<suffix>[a-zA-z0-9\-\+\_\\]+))?}{1,2})")
+    pattern = re.compile(r"(?:{{1,2}(?:(?P<prefix>[a-zA-Z0-9\-\+\_\\]+){)?(?P<key>[a-zA-Z0-9\-]*)(?:\=(?P<extension>\.[a-zA-Z0-9]+))?(?:}(?P<suffix>[a-zA-Z0-9\-\+\_\\]+))?}{1,2})")
     
     for match in re.finditer(pattern, template):
         params[match.group("key")] = match
